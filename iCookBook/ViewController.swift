b2daaba29c962d2235270a1dc4971fff646c0810
Anna Luchechko <annaluchechko@gmail.com>
2020-04-30 22:52:02 +0300

    Create clean project

---
+++ iCookBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCookBook
+//
+//  Created by Anna Luchechko on 30.04.2020.
+//  Copyright © 2020 Anna Luchechko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
