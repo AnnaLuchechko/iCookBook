397321d369b68e93617f34332d505130f08f7201
Anna Luchechko <annaluchechko@gmail.com>
2020-05-04 04:13:43 +0300

    Create first screen with TabBarController and items ViewControllers

--- iCookBook/DiscoverViewController.swift
+++ iCookBook/DiscoverViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ViewController.swift
+//  DiscoverViewController.swift
 //  iCookBook
 //
-//  Created by Anna Luchechko on 30.04.2020.
+//  Created by Anna Luchechko on 04.05.2020.
 //  Copyright © 2020 Anna Luchechko. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DiscoverViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
