a691cc4cbb07c3927032c809a4d3041be75b63f3
Anna Luchechko <annaluchechko@gmail.com>
2020-05-11 23:56:02 +0300

    Add recipeViewController

---
+++ iCookBook/RecipeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeViewController.swift
+//  iCookBook
+//
+//  Created by Anna Luchechko on 11.05.2020.
+//  Copyright © 2020 Anna Luchechko. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
