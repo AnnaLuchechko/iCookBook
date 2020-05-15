6bc717bce6e42f3101114b3d11a706c2288beb5f
Anna Luchechko <annaluchechko@gmail.com>
2020-05-16 00:14:38 +0300

    Fix PreparationSteps

--- iCookBook/RecipeViewController.swift
+++ iCookBook/RecipeViewController.swift
@@ -96,27 +96,30 @@ class RecipeViewController: UIViewController {
         preparationLabel.frame.size = CGSize(width: self.view.frame.size.width - 10, height: 30)
         recipeScrollView.addSubview(preparationLabel)
         
+        labelOriginY = preparationLabel.frame.origin.y + preparationLabel.frame.size.height
+        
         let preparationSteps = recipes?.analyzedInstructions[0].steps
         if let steps = preparationSteps {
             for step in steps {
                 let textBackground = UIView()
                 textBackground.frame.size.width = self.view.frame.size.width - 10
-                textBackground.frame.size.height = 30
+                textBackground.frame.size.height = 60
                 textBackground.frame.origin.x = 5
                 textBackground.frame.origin.y = labelOriginY
                 labelOriginY += 30
                 //textBackground.backgroundColor = .cyan
                 
                 let label = UILabel()
-                label.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width - self.view.frame.size.width/3 - 5, height: 30)
-                label.text = step.name
+                label.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width / 5, height: 30)
+                label.text = String(step.number)
                 label.textAlignment = .left
                 
                 let countLabel = UILabel()
-                countLabel.frame = CGRect(x: label.frame.size.width, y: 0, width: self.view.frame.size.width/3 - 5, height: 30)
-                let countLabelText = String(format: "%.1f", ingridient.amount) + " " + ingridient.unitShort
+                countLabel.frame = CGRect(x: label.frame.size.width, y: 0, width: self.view.frame.size.width - self.view.frame.size.width / 5, height: 30)
+                let countLabelText = step.step
                 countLabel.text = countLabelText
-                countLabel.textAlignment = .right
+                countLabel.textAlignment = .left
+                countLabel.numberOfLines = 2
                 
                 textBackground.addSubview(label)
                 textBackground.addSubview(countLabel)
